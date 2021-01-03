05546aa59b7826db3015091c180a1b0bfbcf7980
Evgeny Gunko <evgeny.gunko@gmail.com>
2021-01-03 19:38:43 +0000

    Merged PR 148: Create Login component

---
+++ Client/Components/Login.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+
+namespace Client.Components
+{
+    public partial class Login
+    {
+        [Inject]
+        private IConfiguration Configuration { get; set; }
+
+        public string AuthorizationUrl
+        {
+            get
+            {
+                string authorizationUri = "http://www.strava.com/oauth/authorize";
+                return authorizationUri + $"?client_id={Configuration["OAuthClientId"]}&response_type=code&redirect_uri={Configuration["OAuthRedirectUri"]}&approval_prompt=auto&scope=read";
+            }
+        }
+    }
+}
