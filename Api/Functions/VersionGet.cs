ec511ca93af8e95a17a21cb10b11524452625935
Evgeny Gunko <evgeny.gunko@gmail.com>
2021-01-03 18:51:33 +0000

    Merged PR 147: Add a new function VersionGet
    Add a new function VersionGet

---
+++ Api/Functions/VersionGet.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Functions
+{
+    public class VersionGet
+    {
+        [FunctionName("VersionGet")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "version")] HttpRequest req,
+            ILogger log)
+        {
+            string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            return new OkObjectResult(version);
+        }
+    }
+}
