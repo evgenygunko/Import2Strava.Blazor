29a42a479c3299a6bba149acc1119bb7f8da7fc1
Evgeny Gunko <evgeny.gunko@gmail.com>
2021-01-11 21:45:50 +0100

    Dynamically construct redirectUri

--- Client/Components/LinkStravaAccount.razor.cs
+++ Client/Components/LinkStravaAccount.razor.cs
@@ -8,12 +8,18 @@ namespace Client.Components
         [Inject]
         private IConfiguration Configuration { get; set; }
 
+        [Inject]
+        private NavigationManager NavManager { get; set; }
+
         public string AuthorizationUrl
         {
             get
             {
+                // Will be something like "https://witty-dune-0fc04b403.azurestaticapps.net/exchange_token"
+                string redirectUri = NavManager.ToAbsoluteUri("exchange_token").ToString();
+
                 string authorizationUri = "http://www.strava.com/oauth/authorize";
-                return authorizationUri + $"?client_id={Configuration["OAuthClientId"]}&response_type=code&redirect_uri={Configuration["OAuthRedirectUri"]}&approval_prompt=auto&scope=read";
+                return authorizationUri + $"?client_id={Configuration["OAuthClientId"]}&response_type=code&redirect_uri={redirectUri}&approval_prompt=auto&scope=read";
             }
         }
     }
