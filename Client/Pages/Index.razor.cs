6e3b035e16c3ea0d3e67b8ad10fbdd2490a09c0f
Evgeny Gunko <evgeny.gunko@gmail.com>
2021-01-25 20:07:05 +0000

    Merged PR 166: Move user info on home page
    Move user info on home page
    
    Related work items: #84

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,6 +1,51 @@
-﻿namespace Client.Pages
+﻿using System;
+using System.Threading.Tasks;
+using Client.Services;
+using Microsoft.AspNetCore.Components;
+using Shared.Models;
+
+namespace Client.Pages
 {
     public partial class Index
     {
+        [Inject]
+        public IDataService DataService { get; set; }
+
+        public UserInfoModel User { get; set; }
+
+        public string Error { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadUserInfoAsync();
+        }
+
+        private async Task UnlinkStravaAppAsync()
+        {
+            try
+            {
+                Error = null;
+                await DataService.UnlinkStravaAppAsync();
+
+                User.IsStravaAccountLinked = false;
+            }
+            catch (Exception ex)
+            {
+                Error = "An error occurred: " + ex;
+            }
+        }
+
+        private async Task LoadUserInfoAsync()
+        {
+            try
+            {
+                Error = null;
+                User = await DataService.GetUserInfoAsync();
+            }
+            catch (Exception ex)
+            {
+                Error = "An error occurred: " + ex;
+            }
+        }
     }
 }
