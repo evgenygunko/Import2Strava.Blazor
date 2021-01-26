850e343a0933f27b8c94537f27661b2028879c00
Evgeny Gunko <evgeny.gunko@gmail.com>
2021-01-26 19:59:25 +0000

    Merged PR 168: Add control for selecting file for upload
    Add control for selecting file for upload
    
    Related work items: #86

--- Client/Pages/ImportWorkouts.razor.cs
+++ Client/Pages/ImportWorkouts.razor.cs
@@ -1,6 +1,66 @@
-﻿namespace Client.Pages
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MatBlazor;
+
+namespace Client.Pages
 {
-    public partial class ApiVersion
+    public partial class ImportWorkouts
     {
+        public string Error { get; set; }
+
+        public string SelectedFile { get; set; }
+
+        public bool IsFileNotSelected { get => string.IsNullOrEmpty(SelectedFile); }
+
+        public bool IsUploading { get; set; }
+
+        public bool IsParsing { get; set; }
+
+        private void FilesReady(IMatFileUploadEntry[] files)
+        {
+            Error = null;
+            SelectedFile = null;
+
+            IMatFileUploadEntry file = files.FirstOrDefault();
+            if (file == null)
+            {
+                return;
+            }
+
+            if (!file.Name.EndsWith(".zip", System.StringComparison.OrdinalIgnoreCase))
+            {
+                Error = "Only zip file can be uploaded";
+            }
+
+            SelectedFile = file.Name;
+        }
+
+        private async Task UploadArchiveAsync()
+        {
+            IsUploading = true;
+            StateHasChanged();
+
+            //try
+            //{
+            //    Error = null;
+            //    await DataService.UnlinkStravaAppAsync();
+
+            //    User.IsStravaAccountLinked = false;
+            //}
+            //catch (Exception ex)
+            //{
+            //    Error = "An error occurred: " + ex;
+            //}
+            await Task.Delay(5000);
+
+            IsUploading = false;
+
+            IsParsing = true;
+            StateHasChanged();
+
+            await Task.Delay(5000);
+
+            IsParsing = false;
+        }
     }
 }
